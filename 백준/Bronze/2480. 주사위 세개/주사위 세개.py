18d70e7e4014cc1f3eca0ccb6ae324e87a06853f
BaekJ02 <wndyd0501ad@gmail.com>
2025-05-16 00:09:46 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -4,7 +4,9 @@ x.sort()
 
 if (x[0] == x[1] and x[0] == x[2]):
     print(10000 + x[0]*1000)
-elif (x[0] == x[1] and x[1] != x[2]) or (x[1] == x[2] and x[0] != x[1]):
+elif (x[0] == x[1]) or (x[1] == x[2]):
+    print(1000+ x[1] * 100)
+elif x[0] == x[2]:
     print(1000+ x[1] * 100)
 else:
     print(max(x) * 100)
